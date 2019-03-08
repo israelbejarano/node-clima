8ea25d3aaa270d3690000c2cdde7ca095f0752f7
israel <israel.bejarano.a@gmail.com>
2019-03-08 23:44:58 +0100

    commit inicial

---
+++ app.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'direccion de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
