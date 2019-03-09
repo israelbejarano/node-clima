509390aba36c8c21d99c14c1be8602b28079bfba
israel <israel.bejarano.a@gmail.com>
2019-03-09 13:00:11 +0100

    main program

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 /*jshint esversion: 6 */
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+const axios = require('axios');
+const colors = require('colors');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,3 +10,18 @@ const argv = require('yargs').options({
         demand: true
     }
 }).argv;
+
+const getInfo = async(direccion) => {
+    try {
+        const coordenadas = await (lugar.getLugarLatLong(direccion));
+        const temperatura = await (clima.getClima(coordenadas.lat, coordenadas.long));
+        return `El clima de ${coordenadas.direccion} es de ${temperatura} C.`
+    } catch (error) {
+        return `No se pudo determinar el tiempo de ${direccion}.`
+    }
+
+}
+
+getInfo(argv.direccion)
+    .then(console.log)
+    .catch(console.log);
